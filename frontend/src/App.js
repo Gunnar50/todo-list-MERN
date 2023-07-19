b812d1b065b2327c9e018dedb3cabe163914a59d
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-07-19 18:17:19 +0100

    apibase

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import {useState, useEffect} from "react";
 
 
 
-const API_BASE = "http://localhost:4000"
+const API_BASE = "https://todo-list-mern-peach.vercel.app/"
 
 function App() {
     const [todos, setTodos] = useState([]);
