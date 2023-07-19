98ab860db00fd13cff47e2aa5b741169697e4ba0
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-07-19 18:42:37 +0100

    cors

--- api/server.js
+++ api/server.js
@@ -6,13 +6,10 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.json());
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
-app.use(cors());
+
+app.use(cors({
+    origin: "https://todo-list-mern-client.vercel.app"
+}));
 
 
 // connect to database
