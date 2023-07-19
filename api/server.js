8fa2f10666e5556cac64ad7afc752b78e796ca57
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-07-19 12:36:45 +0100

    finished server

--- api/server.js
+++ api/server.js
@@ -12,10 +12,35 @@ app.use(cors());
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {console.log("Connected to DB");}).catch((err) => console.log("error", err))
 
+// get all items
 const Todo = require("./models/Todo")
 app.get("/todos", async (req, res) => {
     const todos = await Todo.find();
     res.json(todos);
+})
+
+// create new items
+app.post("/todo/new", (req, res) => {
+    const todo = new Todo({
+        text: req.body.text
+    });
+    todo.save();
+    res.json(todo);
+})
+
+// delete an item by id
+app.delete("/todo/delete/:id", async (req, res) => {
+    const result = await Todo.findByIdAndDelete(req.params.id);
+    
+    res.json(result);
+})
+
+// update item turn to complete
+app.put("/todo/complete/:id", async (req, res) => {
+    const item = await Todo.findById(req.params.id);
+    item.complete = !item.complete;
+    item.save();
+    res.json(item);
 })
 
 // start
