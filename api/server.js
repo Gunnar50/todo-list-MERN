fccfb67beebe666929f3419f585cad3c5ed13878
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-07-19 18:30:20 +0100

    header

--- api/server.js
+++ api/server.js
@@ -8,6 +8,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  });
+
 // connect to database
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {console.log("Connected to DB");}).catch((err) => console.log("error", err))
